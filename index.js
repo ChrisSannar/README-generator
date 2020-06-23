ab22d3e312b79a10bd8cd4b48f826f779fc97e11
Christopher Sannar <Christopher.sannar@gmail.com>
2020-06-22 18:18:56 -0600

    Added inquirer prompts
    Included various prompts to ask the user about the README document we're generating.

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description?',
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the installation instructions?',
+        default: 'npm install',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the directions to use the project?',
+        default: 'npm start'
+    },
+    {
+        type: 'list',
+        name: 'licence',
+        message: 'What licence is the project under?',
+        default: 'MIT',
+        choices: ['MIT', 'GLP']
+    },
+    {
+        type: 'input',
+        name: 'contrib',
+        message: 'Who are the authors of this project? (insert names seperated by commas)'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How do you test the project?',
+        default: 'npm test'
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFileSync('./output/' + fileName, data);
 }
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            console.log(answers);
+            // writeToFile('README.md', 'testing');
+        });
 }
 
 // function call to initialize program
 init();
-
-console.log('working');
